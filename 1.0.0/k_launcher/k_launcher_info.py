0b7e0ffb579d748d4736af27d7144a4b20631e4b
Felix Benicourt <33630000+felixBenicourt@users.noreply.github.com>
2024-12-30 20:15:23 -0500

    Update k_launcher_info.py

--- 1.0.0/k_launcher/k_launcher_info.py
+++ 1.0.0/k_launcher/k_launcher_info.py
@@ -18,7 +18,7 @@ def print_k_launcher_documentation_rez():
 
     Arguments (args*):
         -c, --config : name of the config you're working on.
-        -co, --context : Display the current context.
+        -co, --context : display the current context.
         -p, --package : package to load with the environment.
         -a, --add : additional packages to be added.
         -l, --launch : launch the DCC software by name.
