df0c731fc818d2d22f144523e684992fb971fc4a
Felix Benicourt <33630000+felixBenicourt@users.noreply.github.com>
2024-12-31 17:48:36 -0500

    Add files via upload

---
+++ 1.0.0/k_launcher/k_launcher_git.py
@@ -0,0 +1,205 @@
+
+
+import argparse
+import logging
+import sys
+import os
+from k_constants import CONSTANTS
+import k_launcher_info
+import k_launcher_utils
+import k_launcher_git_cmd
+import k_launcher_context
+import k_launcher_id
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+class KLauncher_git(k_launcher_git_cmd.k_git_cmd,
+                    k_launcher_context.PackageContextManager
+                    ):
+    """
+    A class to manage Git operations within the context of a pipeline environment.
+
+    Attributes:
+        package (str): The package name to operate on.
+        branch (str): The Git branch name.
+        path (str): The file path for Git operations.
+        session_id (str): Unique identifier for the current terminal session.
+    """
+
+    def __init__(self):
+        """
+        Initializes the KLauncher_git object, setting up the session ID, repository dictionary,
+        and loading the context from a saved file.
+        """
+        self.package = None
+        self.branch = None
+        self.path = None
+        self.session_id = str(k_launcher_id.get_terminal_pid())
+        self.load_repo_dict()
+        self.load_data_context()
+
+    def parse_args(self):
+        """
+        Parses command-line arguments for Git operations.
+
+        Returns:
+            argparse.Namespace: Parsed arguments object.
+        """
+        parser = argparse.ArgumentParser(description="k_launcher - A script to manage git in the pipeline context.")
+
+        parser.add_argument("-i", "--info", action="store_true", help="Display information about the tool.")
+        parser.add_argument("-p", "--package", type=str, help="Load package")
+        parser.add_argument("-co", "--context", action="store_true", help="Display the current context")
+        parser.add_argument("-gc", "--git_clone", action="store_true", help="GIT clone command.")
+        parser.add_argument("-gf", "--git_fetch", action="store_true", help="GIT fetch command.")
+        parser.add_argument("-gp", "--git_pull", action="store_true", help="GIT pull command.")
+        parser.add_argument("-ch", "--git_check", action="store_true", help="GIT checkout command.")
+        parser.add_argument("-c", "--git_commit", action="store_true", help="GIT commit command.")
+        parser.add_argument("-cr", "--git_create", action="store_true", help="GIT create branch command.")
+        parser.add_argument("-gl", "--git_list", action="store_true", help="GIT list command.")
+        parser.add_argument("-log", "--git_log", action="store_true", help="GIT log command.")
+        parser.add_argument("-t", "--git_tag", type=str, help="GIT tag command.")
+        parser.add_argument("-gh", "--history", action="store_true", help="GIT history command.")
+        parser.add_argument("-pa", "--path", type=str, help="Folder path argument.")
+        parser.add_argument("-gu", "--git_url", type=str, help="Git URL for cloning.")
+        parser.add_argument("-b", "--branch", type=str, help="Branch name.")
+        parser.add_argument("-m", "--msg", type=str, help="Message.")
+        parser.add_argument("-vs", "--vs_code", action="store_true", help="launch vs code with the path and package")
+
+        return parser.parse_args()
+
+    def set_arguments(self, args):
+        """
+        Sets command-line arguments as instance variables and updates the context file accordingly.
+
+        Args:
+            args (argparse.Namespace): Parsed command-line arguments.
+        """
+        if args.package:
+            self.package = args.package
+            self.save_data_context("package", self.package)
+        if args.branch:
+            self.branch = args.branch
+            self.save_data_context("branch", self.branch)
+        if "path" in self.load_data_context():
+            if args.path:
+                self.path = args.path
+                self.save_data_context("path", args.path)
+            else:
+                self.path = self.load_data_context()["path"]
+        else:
+            if args.path:
+                self.save_data_context("path", args.path)
+            else:
+                self.path = args.path or CONSTANTS.rootLocalFolder
+                self.save_data_context("path", self.path)
+
+    def execute_commands(self, args):
+        """
+        Executes Git operations based on parsed command-line arguments.
+
+        Args:
+            args (argparse.Namespace): Parsed command-line arguments.
+        """
+        try:
+            if args.info:
+                k_launcher_info.print_k_launcher_documentation_git()
+
+            if args.vs_code:
+                k_launcher_utils.launch_vs_with_package(os.path.join(self.path, self.package))
+
+            if args.context:
+                self.get_data_context()
+
+            if args.git_clone:
+                if args.git_url and args.path:
+                    self.clone_repository(custom_url=args.git_url, path=args.path)
+                elif self.package and self.path:
+                    self.clone_repository(name=self.package, path=self.path)
+                else:
+                    logging.error("Missing required arguments for git clone: --git_url and --path or a valid context (package/path).")
+                    sys.exit(1)
+
+            if args.git_fetch:
+                if self.package and self.path:
+                    self.fetch_repository(self.package, self.path, self.package)
+                else:
+                    logging.error("Missing package or path for git fetch.")
+                    sys.exit(1)
+
+            if args.git_pull:
+                if self.package and self.path:
+                    self.pull_repository(self.path, self.package)
+                else:
+                    logging.error("Missing package or path for git pull.")
+                    sys.exit(1)
+
+            if args.git_check:
+                if self.package and self.branch:
+                    self.checkout_branch(self.path, self.package, self.branch)
+                else:
+                    logging.error("Missing package or branch for git checkout.")
+                    sys.exit(1)
+
+            if args.git_commit and args.msg:
+                if self.package and self.path:
+                    self.commit_repository(self.path, self.package, args.msg)
+                else:
+                    logging.error("Missing package or path for git commit.")
+                    sys.exit(1)
+
+            if args.git_create:
+                if self.package and args.git_tag:
+                    self.create_branch(self.path, self.package, self.package, args.git_tag)
+                else:
+                    logging.error("Missing package or git tag for git branch creation.")
+                    sys.exit(1)
+
+            if args.git_list:
+                if self.package and self.path:
+                    self.list_remote_branches(self.path, self.package)
+                else:
+                    logging.error("Missing package or path for git list remote branches.")
+                    sys.exit(1)
+
+            if args.git_log:
+                if self.package and self.path:
+                    self.show_commit_log(self.path, self.package)
+                else:
+                    logging.error("Missing package or path for git log.")
+                    sys.exit(1)
+
+            if args.history:
+                if self.package and self.path:
+                    self.list_repository_history(self.path, self.package)
+                else:
+                    logging.error("Missing package or path for git history.")
+                    sys.exit(1)
+
+            if args.git_tag and args.git_tag:
+                if self.package and self.path:
+                    self.tag_repository(self.path, self.package, args.git_tag)
+                else:
+                    logging.error("Missing package or path for git tag.")
+                    sys.exit(1)
+
+        except Exception as e:
+            logging.error(f"An error occurred: {e}", exc_info=True)
+            sys.exit(1)
+
+
+def main():
+    """
+    Main function to initialize and run the KLauncher_git.
+    """
+    launcher = KLauncher_git()
+    args = launcher.parse_args()
+    launcher.set_arguments(args)
+    launcher.execute_commands(args)
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ 1.0.0/k_launcher/k_launcher_info.py
@@ -0,0 +1,140 @@
+
+
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+def print_k_launcher_documentation_rez():
+    """
+    Prints the documentation for k_launcher in a single log statement.
+    """
+    documentation = """
+    This is the documentation for the k_launcher Rez Functionality.
+
+    Usage:
+        python k_launcher_rez.py [args*]
+
+    Arguments (args*):
+        -c, --config : name of the config you're working on.
+        -co, --context : display the current context.
+        -p, --package : package to load with the environment.
+        -a, --add : additional packages to be added.
+        -l, --launch : launch the DCC software by name.
+        -s, --save : save the context/configuration.
+        -lo, --load : load the saved context/configuration.
+        -i, --info : display information about the tool.
+        -e, --echo : display the current settings.
+        -g, --grab : grab the package in PROD to LOCAL.
+        -w, --switch : switch the package to the local version.
+        -r, --release : chosen LOCAL package to release.
+        -pr, --prod_release : chosen version of the package to release on PROD.
+        -vs, --vs_code : launch vs code with the path and package.
+
+    Example Launch Commands:
+        python k_launcher_rez.py --info
+        python k_launcher_rez.py --config dev --launch maya --add myPackage
+        python k_launcher_rez.py --save devConfig
+        python k_launcher_rez.py --load prodConfig
+
+    Config Structure:
+        config : package : context/path/file.rxt
+
+    Class KLauncher_rez:
+        The KLauncher_rez class manages the environment setup and execution of DCC software.
+        It handles various tasks such as setting and displaying configuration details,
+        managing the environment variables using `rez`, and launching DCC software with
+        the specified packages and settings.
+
+        Attributes:
+            - config_set (str): The configuration set to use.
+            - package (str): The main package to load with the environment.
+            - add_package (str): Additional packages to be added to the environment.
+            - dcc_launch (str): The DCC software to launch.
+            - save_config (str): The configuration to save.
+            - load_config (str): The configuration to load.
+            - grab_commande (list): List of packages to grab.
+            - switch_commande (list): List of packages to switch.
+
+        Methods:
+            - echo_settings(): Logs the current configuration settings for the KLauncher_rez instance.
+            - eval_rez_command(): Executes the generated `rez` command to set up the environment.
+    """
+
+    logging.info(documentation)
+
+
+def print_k_launcher_documentation_git():
+    """
+    Prints the documentation for k_launcher in a single log statement.
+    """
+    documentation = """
+    Documentation for k_launcher Git Functionality:
+
+    Usage:
+        python k_launcher_git.py [args*]
+
+    Arguments (args*):
+        -i, --info : Display information about the tool.
+        -p, --package : Load package.
+        -co, --context : Display the current context.
+        -gc, --git_clone : Clone a Git repository.
+            Parameters:
+                - URL of the repository.
+                - Local folder to clone into.
+                - Optional name for the repository folder.
+        -gf, --git_fetch : Fetch the latest changes from the remote repository.
+            Parameters:
+                - Path to the local folder containing the repository.
+                - Name of the repository.
+        -gp, --git_pull : Update the repository by pulling the latest changes from the remote.
+            Parameters:
+                - Path to the local folder containing the repository.
+                - Name of the repository.
+        -ch, --git_check : Switch to a specific branch in the repository.
+            Parameters:
+                - Path to the local folder containing the repository.
+                - Name of the repository.
+                - Name of the branch to checkout.
+        -c, --git_commit : Commit changes to the repository.
+            Parameters:
+                - Path to the local folder containing the repository.
+                - Name of the repository.
+                - Commit message.
+        -cr, --git_create : Create a new branch in the specified repository.
+            Parameters:
+                - Path to the local folder containing the repository.
+                - Name of the repository.
+                - Name of the new branch.
+        -gl, --git_list : List remote branches of the repository.
+            Parameters:
+                - Path to the local folder containing the repository.
+                - Name of the repository.
+        -log, --git_log : Display the commit log for the repository.
+            Parameters:
+                - Path to the local folder containing the repository.
+                - Name of the repository.
+        -t, --git_tag : Tag the repository.
+            Parameters:
+                - Path to the local folder containing the repository.
+                - Name of the repository.
+        -gh, --history : Display the history of the repository.
+            Parameters:
+                - Path to the local folder containing the repository.
+                - Name of the repository.
+        -pa, --path : Folder path argument.
+        -gu, --git_url : Git URL for cloning.
+        -b, --branch : Branch name.
+        -m, --msg : Commit message.
+        -vs, --vs_code : launch vs code with the path and package.
+
+    Description:
+        The Git functionality of `k_launcher` is designed to simplify the process of managing Git repositories. 
+        It provides commands for common Git operations such as cloning repositories, creating and switching branches, 
+        and synchronizing with remote repositories. These commands help streamline version control tasks 
+        within the context of a production pipeline.
+    """
+
+    logging.info(documentation)
+
